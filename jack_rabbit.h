877f947d80d6f14fdf3f1134b154d9bda1cf6b76
Jeff Perry <jeffsp@gmail.com>
2012-03-30 12:54:39 -0500

    move foveation stuff to horny_toad

--- jack_rabbit.h
+++ jack_rabbit.h
@@ -1,20 +1,17 @@
-// Copyright (C) 2009
-// Center for Perceptual Systems
+// Pyramid Container
 //
-// jsp Wed Mar 25 14:53:24 CDT 2009
+// Copyright (C) 2003-2012 Jeffrey S. Perry
+//
+// contact: jeffsp@gmail.com
 
 #ifndef JACK_RABBIT_H
 #define JACK_RABBIT_H
 
-#include "blending_map.h"
-#include "foveator.h"
 #include "pyramid.h"
 #include "raster.h"
-#include "rect.h"
 #include "subregion_algo.h"
 #include "subregion.h"
 #include "subregion_iter.h"
 #include "subscript_function.h"
-#include "superpixel.h"
 
 #endif // JACK_RABBIT_H
